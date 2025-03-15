7267f1fbd3d122bd0e94c4dd966f2b79d6b1e024
heaven.venorg <heavensipangkar1@gmail.com>
2025-03-15 16:56:43 +0700

    update

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Wolrd API Native</title>
+    <!-- Font Poppins -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <style>
+    body {
+        font-family: "Poppins", sans-serif;
+        height: 100vh;
+        background-color: whitesmoke;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        line-height: 0px
+    }
+    </style>
+</head>
+
+<body>
+    <div>
+        <h1>PHP Native API</h1>
+        <p>Learn in here API PHP Native</p>
+    </div>
+</body>
+
+</html>
